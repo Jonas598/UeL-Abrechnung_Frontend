4d80b43ba970ad95fd9fc8955d70af6df09c02c0
Jonas598 <jonas.richter@alumni.fh-aachen.de>
2025-11-15 16:25:21 +0100

    "V5.0.2 Name zu Drumm geaendert"

--- my-app/src/components/HelloWorld.vue
+++ my-app/src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">Jonas ist {{ count }} Jahre alt</button>
+    <button type="button" @click="count++">Drumm ist {{ count }} Jahre alt</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
